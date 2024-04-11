8326d5b39b8bc2d74fdc0708a9fe017cb942aa7c
Ryan Le <ryanle@live.com>
2024-04-11 17:42:22 -0400

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
-import './globals.css';
+// Components
+import { Analytics } from '@vercel/analytics/react';
+
+// Utilities
 import { Inter } from 'next/font/google';
+import './globals.css';
 import seo from '@/seo.config';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 };
